2a81930b149bea667a243e29187940677e739a1b
ValeAyala08 <115433571+ValeAyala08@users.noreply.github.com>
2025-05-07 16:50:03 +0000

    creacion de carpetas

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Proyecto_integrador_5",
+    version="0.0.1",
+    author="Valentina Ayala",
+    author_email="",
+    description="",
+    py_modules=["actividad_1","actividad_2"],
+    install_requires=[
+        "pandas==2.2.3",
+        "openpyxl",
+        "requests==2.32.3"
+    ]
+) 
